7b25a5c95fc2d4096dbb8787689d7e5aed723b1e
Miloš Zmijanjac <zmijanjac@gmail.com>
2022-07-16 22:33:13 +0200

    Create GoogleSignIn.svelte

---
+++ GoogleSignIn.svelte
@@ -0,0 +1,22 @@
+<head>
+  <script src="https://accounts.google.com/gsi/client" async defer></script>
+</head>
+
+<script>
+  export let clientId,jwtoken;
+  function handleCredentialResponse(response) {
+    jwtoken=response.credential
+  }
+  window.onload = function () {
+    google.accounts.id.initialize({
+      client_id: clientId,
+      callback: handleCredentialResponse
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("buttonDiv"),
+      { theme: "outline", size: "large" }  // customization attributes
+    );
+    google.accounts.id.prompt(); // also display the One Tap dialog
+  }
+</script>
+<div id="buttonDiv"></div> 
